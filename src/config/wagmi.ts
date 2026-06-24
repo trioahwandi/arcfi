31ebc8dcae449503d9bd71f40a7f6070b9afb01b
Velo 3.0 <trio.ahwandi13@gmail.com>
2026-06-25 01:35:28 +0700

    Deploy from Zero Studio

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -1,34 +1,36 @@
-import { http, createConfig } from "wagmi"
-import { injected, metaMask, coinbaseWallet } from "wagmi/connectors"
-import { arcTestnet } from "./chains"
+// Arc Testnet Configuration
+export const ARC_CHAIN_ID = 5042002;
+export const ARC_CHAIN_ID_HEX = '0x4D59E6';
 
-
-export const config = createConfig({
-
-  chains: [
-    arcTestnet
-  ],
-
-  connectors: [
-    injected(),
-
-    metaMask(),
-
-    coinbaseWallet({
-      appName: "ArcFi",
-    }),
-  ],
-
-  transports: {
-    [arcTestnet.id]: http(
-      "https://rpc.testnet.arc.network"
-    ),
+export const arcTestnet = {
+  id: ARC_CHAIN_ID,
+  name: 'Arc Testnet',
+  nativeCurrency: {
+    name: 'USDC',
+    symbol: 'USDC',
+    decimals: 18,
   },
-})
+  rpcUrls: {
+    default: {
+      http: ['https://rpc.testnet.arc.network'],
+    },
+    public: {
+      http: ['https://rpc.testnet.arc.network'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'ArcScan',
+      url: 'https://testnet.arcscan.app',
+    },
+  },
+  testnet: true,
+};
 
+export const config = {};
 
-declare module "wagmi" {
-  interface Register {
-    config: typeof config
+declare module 'wagmi' {
+  export interface Register {
+    config: typeof config;
   }
 }
